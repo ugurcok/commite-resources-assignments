473e30b8a111f3e08abed2536542cfab8076f029
Emre Samurlu <emrsmrl@gmail.com>
2022-03-05 22:49:35 +0300

    Full Stack Proje 2. Hafta | JS Counter App

--- index.js
+++ index.js
@@ -1,45 +1,28 @@
-const myHeading = document.querySelector("#title");
-myHeading.textContent = "Full Stack Project Group! Hello!!!";
+let counter = 0;
 
-const myNumber = document.querySelector("#number");
-myNumber.textContent = "8";
+const value = document.querySelector("#value");
+const buttons = document.querySelectorAll(".btn");
 
-console.log(typeof myHeading.textContent);
-console.log(typeof myNumber.textContent);
+buttons.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      counter--;
+    } else if (styles.contains("increase")) {
+      counter++;
+    } else {
+      counter = 0;
+    }
 
-console.log(4 !== 4);
-
-// let iceCream = "chocolate";
-
-// if (iceCream === "vanilla") {
-//   alert("Yay, I love chocolate ice cream!");
-// } else {
-//   alert("Awwww, but chocolate is my favorite...");
-// }
-
-// let num1 = 5;
-// let num2 = 4;
-
-function multiply1(num1, num2) {
-  let result = num1 * num2;
-  return result;
-}
-console.log("multiply1", multiply1(4, 4));
-
-const multiply2 = (num1, num2) => {
-  return num1 * num2;
-};
-console.log("multiply2", multiply2(4, 4));
-
-console.log(
-  "multiply3",
-  (function (num1, num2) {
-    return num1 * num2;
-  })(3, 4)
-);
-
-const multiply4 = function (num1, num2) {
-  return num1 * num2;
-};
-
-console.log("multiply4", multiply4(4, 4));
+    if (counter > 0) {
+      value.style.color = "green";
+    }
+    if (counter < 0) {
+      value.style.color = "red";
+    }
+    if (counter === 0) {
+      value.style.color = "blue";
+    }
+    value.textContent = counter;
+  });
+});
